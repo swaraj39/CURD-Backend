5008c73d2cd603cea0371dbce69be727436db4ff
swaraj39 <162737985+swaraj39@users.noreply.github.com>
2026-02-12 14:41:23 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     // Allows requests from the specified frontend origin
-                    config.addAllowedOrigin("http://localhost:3003");
+                    config.addAllowedOrigin("https://crud-frontend-yb8t.vercel.app");
                     // Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
                     config.addAllowedMethod("*");
                     // Allows all headers in requests
